766940b8f6c8509c69bfca5d9ca60b4f37a030ea
Jopannnn <jov132009@gmail.com>
2026-03-22 12:29:38 +0700

    Menyelesaikan halaman classes

---
+++ app/views/students/classes.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes</title>
+    <link rel="stylesheet" href="/public/css/classes.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+
+<body>
+
+    <div class="sidebar">
+
+        <div class="Top">
+            <div class="Logo-utama">
+                <img src="/public/assets/Group-82.png" alt="Logo" style="width: 50px; height: 65px;">
+            </div>
+
+            <div class="menu">
+                <div class="icon"><i class="fa-solid fa-image"></i></div>
+                <div class="icon"><i class="fa-solid fa-bullhorn"></i></div>
+                <div class="icon"><i class="fa-solid fa-comment-dots"></i></div>
+                <div class="icon active"><i class="fa-solid fa-user"></i></div>
+            </div>
+        </div>
+
+
+        <div class="bottom">
+            <div class="icon"><i class="fa-solid fa-gear"></i></div>
+        </div>
+
+    </div>
+
+
+    <!-- ================= MAIN ================= -->
+    <div class="main">
+
+        <!-- ================= TOPBAR ================= -->
+        <div class="topbar">
+
+            <!-- SEARCH BAR -->
+            <div class="search-box">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="text" placeholder="Search...">
+            </div>
+
+            <!-- PROFILE -->
+            <div class="profile">
+                <div class="status-icon box"></div>
+
+                <span>10 June - 23 June</span>
+                <div class="status-icon circle"></div>
+
+                <img src="/public/assets/Ellipse-30.png" alt="Profile Picture"
+                    style="width: 40px; height: 40px; border-radius: 50%;">
+                <span>Lmaopisang</span>
+            </div>
+
+        </div>
+
+        <!-- ================= HEADER ================= -->
+        <div class="header">
+            <h2>Your Classes</h2>
+            <button>
+                <i class="fa-solid fa-plus"></i>
+                Join or Create your teams
+            </button>
+        </div>
+
+        <!-- ================= CLASS LIST ================= -->
+        <div class="class-container">
+
+            <!-- CARD -->
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-header-foto">
+                        <img src="/public/assets/Rectangle-1534.png" style="width: 100px; height: 95px;">
+
+                        <div class="card-header">
+                            <h3>Ur Classes</h3>
+                            <h4>Class Description</h4>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+                <div class="line"></div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-header-foto">
+                        <img src="/public/assets/Rectangle-1534(1).png" style="width: 100px; height: 95px;">
+
+                        <div class="card-header">
+                            <h3>Ur Classes</h3>
+                            <h4>Class Description</h4>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+                <div class="line"></div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-header-foto">
+                        <img src="/public/assets/Rectangle-1534(2).png" style="width: 100px; height: 95px;">
+
+                        <div class="card-header">
+                            <h3>Ur Classes</h3>
+                            <h4>Class Description</h4>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+                <div class="line"></div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-header-foto">
+                        <img src="/public/assets/Rectangle-1534(3).png" style="width: 100px; height: 95px;">
+
+                        <div class="card-header">
+                            <h3>Ur Classes</h3>
+                            <h4>Class Description</h4>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+                <div class="line"></div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-header-foto">
+                        <img src="/public/assets/Rectangle-1534(4).png" style="width: 100px; height: 95px;">
+
+                        <div class="card-header">
+                            <h3>Ur Classes</h3>
+                            <h4>Class Description</h4>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+                <div class="line"></div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-header-foto">
+                        <img src="/public/assets/Rectangle-1534(5).png" style="width: 100px; height: 95px;">
+
+                        <div class="card-header">
+                            <h3>Ur Classes</h3>
+                            <h4>Class Description</h4>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+                <div class="line"></div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-header-foto">
+                        <img src="/public/assets/Rectangle-1534(6).png" style="width: 100px; height: 95px;">
+
+                        <div class="card-header">
+                            <h3>Ur Classes</h3>
+                            <h4>Class Description</h4>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+                <div class="line"></div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-header-foto">
+                        <img src="/public/assets/Rectangle-1534(7).png" style="width: 100px; height: 95px;">
+
+                        <div class="card-header">
+                            <h3>Ur Classes</h3>
+                            <h4>Class Description</h4>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+                <div class="line"></div>
+            </div>
+
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-header-foto">
+                        <img src="/public/assets/Rectangle-1534(8).png" style="width: 100px; height: 95px;">
+
+                        <div class="card-header">
+                            <h3>Ur Classes</h3>
+                            <h4>Class Description</h4>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+                <div class="line"></div>
+            </div>
+
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
